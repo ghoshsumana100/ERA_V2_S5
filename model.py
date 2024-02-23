5bfa7ed8b404c37c145519cd3d85ffa3ca11c810
ghoshsumana100 <116897778+ghoshsumana100@users.noreply.github.com>
2024-02-24 04:01:06 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,43 +1,58 @@
-# Train data transformations
-train_transforms = transforms.Compose([
-    transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
-    transforms.Resize((28, 28)),
-    transforms.RandomRotation((-15., 15.), fill=0),
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,)),
-    ])
+class Net(nn.Module):
+    #This defines the structure of the NN.
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
+        self.fc1 = nn.Linear(4096, 50)
+        self.fc2 = nn.Linear(50, 10)
 
-# Test data transformations
-test_transforms = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
-    ])
-
-batch_size = 512
-
-kwargs = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 2, 'pin_memory': True}
-
-test_loader = torch.utils.data.DataLoader(test_data, **kwargs)
-train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
-
-# Data to plot accuracy and loss graphs
-train_losses = []
-test_losses = []
-train_acc = []
-test_acc = []
-
-test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+    def forward(self, x):
+        x = F.relu(self.conv1(x), 2) # 28>26 | 1>3 | 1>1
+        x = F.relu(F.max_pool2d(self.conv2(x), 2)) #26>24>12 | 3>5>6 | 1>1>2
+        x = F.relu(self.conv3(x), 2) # 12>10 | 6>10 | 2>2
+        x = F.relu(F.max_pool2d(self.conv4(x), 2)) # 10>8>4 | 10>14>16 | 2>2>4
+        x = x.view(-1, 4096) # 4*4*256 = 4096
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
 
 
+!pip install torchsummary
+from torchsummary import summary
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+model = Net().to(device)
+summary(model, input_size=(1, 28, 28))   
+
+class Net2(nn.Module):
+    #This defines the structure of the NN.
+    def __init__(self):
+        super(Net2, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, bias=False)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, bias=False)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, bias=False)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, bias=False)
+        self.fc1 = nn.Linear(4096, 50, bias=False)
+        self.fc2 = nn.Linear(50, 10, bias=False)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x), 2) # 28>26 | 1>3 | 1>1
+        x = F.relu(F.max_pool2d(self.conv2(x), 2)) #26>24>12 | 3>5>6 | 1>1>2
+        x = F.relu(self.conv3(x), 2) # 12>10 | 6>10 | 2>2
+        x = F.relu(F.max_pool2d(self.conv4(x), 2)) # 10>8>4 | 10>14>16 | 2>2>4
+        x = x.view(-1, 4096) # 4*4*256 = 4096
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+!pip install torchsummary
+from torchsummary import summary
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+model = Net2().to(device)
+summary(model, input_size=(1, 28, 28))
 
 
-fig, axs = plt.subplots(2,2,figsize=(15,10))
-axs[0, 0].plot(train_losses)
-axs[0, 0].set_title("Training Loss")
-axs[1, 0].plot(train_acc)
-axs[1, 0].set_title("Training Accuracy")
-axs[0, 1].plot(test_losses)
-axs[0, 1].set_title("Test Loss")
-axs[1, 1].plot(test_acc)
-axs[1, 1].set_title("Test Accuracy")
-     
