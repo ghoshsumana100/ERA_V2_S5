182a4897754fb6d1aaa9207c95e60f42335eddd8
ghoshsumana100 <116897778+ghoshsumana100@users.noreply.github.com>
2024-02-24 04:00:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,75 +1,4 @@
-class Net(nn.Module):
-    #This defines the structure of the NN.
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
-        self.fc1 = nn.Linear(4096, 50)
-        self.fc2 = nn.Linear(50, 10)
 
-    def forward(self, x):
-        x = F.relu(self.conv1(x), 2) # 28>26 | 1>3 | 1>1
-        x = F.relu(F.max_pool2d(self.conv2(x), 2)) #26>24>12 | 3>5>6 | 1>1>2
-        x = F.relu(self.conv3(x), 2) # 12>10 | 6>10 | 2>2
-        x = F.relu(F.max_pool2d(self.conv4(x), 2)) # 10>8>4 | 10>14>16 | 2>2>4
-        x = x.view(-1, 4096) # 4*4*256 = 4096
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
-
-!pip install torchsummary
-from torchsummary import summary
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda" if use_cuda else "cpu")
-model = Net().to(device)
-summary(model, input_size=(1, 28, 28))
-
-class Net2(nn.Module):
-    #This defines the structure of the NN.
-    def __init__(self):
-        super(Net2, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, bias=False)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, bias=False)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, bias=False)
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, bias=False)
-        self.fc1 = nn.Linear(4096, 50, bias=False)
-        self.fc2 = nn.Linear(50, 10, bias=False)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x), 2) # 28>26 | 1>3 | 1>1
-        x = F.relu(F.max_pool2d(self.conv2(x), 2)) #26>24>12 | 3>5>6 | 1>1>2
-        x = F.relu(self.conv3(x), 2) # 12>10 | 6>10 | 2>2
-        x = F.relu(F.max_pool2d(self.conv4(x), 2)) # 10>8>4 | 10>14>16 | 2>2>4
-        x = x.view(-1, 4096) # 4*4*256 = 4096
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
-
-!pip install torchsummary
-from torchsummary import summary
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda" if use_cuda else "cpu")
-model = Net2().to(device)
-summary(model, input_size=(1, 28, 28))
-
-
-model = Net().to(device)
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1, verbose=True)
-# New Line
-criterion = F.nll_loss
-num_epochs = 20
-
-for epoch in range(1, num_epochs+1):
-  print(f'Epoch {epoch}')
-  train(model, device, train_loader, optimizer, criterion)
-  test(model, device, test_loader, criterion)
-  scheduler.step()
-
-
- 
 from tqdm import tqdm
 
 def GetCorrectPredCount(pPrediction, pLabels):
@@ -129,4 +58,13 @@ def test(model, device, test_loader, criterion):
     print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-      
+     
+fig, axs = plt.subplots(2,2,figsize=(15,10))
+axs[0, 0].plot(train_losses)
+axs[0, 0].set_title("Training Loss")
+axs[1, 0].plot(train_acc)
+axs[1, 0].set_title("Training Accuracy")
+axs[0, 1].plot(test_losses)
+axs[0, 1].set_title("Test Loss")
+axs[1, 1].plot(test_acc)
+axs[1, 1].set_title("Test Accuracy")
